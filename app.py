9d9f4da1cbd07eea7a8ff14ec4e090fc5cc53001
Fadelk01 <68284235+adletgamer@users.noreply.github.com>
2025-06-04 10:47:44 -0500

    Add files via upload

---
+++ app.py
@@ -0,0 +1,42 @@
+from flask import Flask, jsonify, request 
+from flask_cors import CORS
+
+app = Flask(__name__)
+CORS(app)
+
+infectados = [
+    {"id": 1, "nombre": "Runner", "nivel_peligro": "Bajo", "zona": "Pittsburgh", "es_mutacion": False},
+    {"id": 2, "nombre": "Cliker", "nivel_peligro": "Alto", "zona": "Boston", "es_mutacion": True}
+]
+
+# Método GET /infectados/<id>
+@app.route('/infectados/<int:id>', methods=['GET'])
+def obtener_infectado(id):
+    for i in infectados:
+        if i["id"] == id:
+            return jsonify(i)
+    return {"error": "Infectado no encontrado"}, 404
+
+# POST /infectados
+@app.route('/infectados', methods=['POST'])
+def add_infectados():
+    nuevo = request.json
+    nuevo["id"] = len(infectados) + 1
+    infectados.append(nuevo)
+    return jsonify(nuevo), 201
+
+# PUT /infectados/<id>
+@app.route('/infectados/<int:id>', methods=['PUT'])
+def actualiza_infectado(id):
+    for i in infectados:
+        if i["id"] == id:
+            i.update(request.json)
+            return jsonify(i)
+    return {"error": "Infectado no encontrado"}, 404
+
+# DELETE /infectados/<id>
+@app.route('/infectados/<int:id>', methods=['DELETE'])
+def eliminar_infectados(id):
+    global infectados
+    infectados = [i for i in infectados if i["id"] != id]
+    return {"mensaje": "Infectado eliminado"}, 200
